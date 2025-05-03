44a1d027df14f596449302d8aa5668a25037262c
clangclang1 <enovidadc4@gmail.com>
2025-05-03 15:23:46 +0900

    BACKEND
    -LOGIN
    -REGISTER
    -LOGOUT

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { register } from '../controllers/authControllers.js';
+import { logout, login, register } from '../controllers/authControllers.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
 
 export default authRouter;
